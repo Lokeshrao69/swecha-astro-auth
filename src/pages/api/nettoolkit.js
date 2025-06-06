518fc858029b6888452676308f7e1c03dc570ff6
Lokeshrao69 <lokeshrao226@gmail.com>
2025-06-06 09:41:34 +0530

    first

---
+++ src/pages/api/nettoolkit.js
@@ -0,0 +1,18 @@
+export async function GET() {
+  const apiKey = import.meta.env.NETTOOLKIT_API_KEY;
+
+  const res = await fetch("https://api.nettoolkit.com/v1/account/test-api-keys", {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
